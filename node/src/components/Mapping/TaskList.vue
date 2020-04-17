cb6888e7948c53f450bcaa2eb052bd20db8799bb
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-04-17 23:45:24 +0200

    Labels for filters

--- node/src/components/Mapping/TaskList.vue
+++ node/src/components/Mapping/TaskList.vue
@@ -20,13 +20,13 @@
                             <v-checkbox v-model="filterOnUser"></v-checkbox>
                         </v-col>
                         <v-col cols=11>
-                            <v-select class="pa-1" :items="users" v-model="filterUser"></v-select>
+                            <v-select class="pa-1" :items="users" v-model="filterUser" label="Filter op gebruiker"></v-select>
                         </v-col>
                         <v-col cols=1>
                             <v-checkbox v-model="filterOnStatus"></v-checkbox>
                         </v-col>
                         <v-col cols=11>
-                            <v-select class="pa-1" :items="statuses" v-model="filterStatus"></v-select>
+                            <v-select class="pa-1" :items="statuses" v-model="filterStatus" label="Filter op status"></v-select>
                         </v-col>
                     </v-row>
                 </v-card-text>
