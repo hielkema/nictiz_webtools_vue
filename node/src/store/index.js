9785ccdf893dcb9bda3f6b084f7932f89a0b6d68
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-04-23 14:59:38 +0200

    oops

--- node/src/store/index.js
+++ node/src/store/index.js
@@ -33,8 +33,8 @@ Vue.use(axios)
 
 export default new Vuex.Store({
   state: {
-    // baseUrl: 'https://termservice.test-nictiz.nl/',
-    baseUrl: 'http://localhost:8000/',
+    baseUrl: 'https://termservice.test-nictiz.nl/',
+    // baseUrl: 'http://localhost:8000/',
     userData: {
       'id': null,
       'groups' : [],
