112702c2bfa80c9c24bdf7acdd913b3949071997
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-05-08 23:11:57 +0200

    Audit improvement

--- node/src/components/Mapping/AuditHitsPerProject.vue
+++ node/src/components/Mapping/AuditHitsPerProject.vue
@@ -10,6 +10,54 @@
             </v-row>
             <v-row>
                 <v-col cols=12>
+                    <v-card>
+                        <v-card-title>
+                            Taken
+                        </v-card-title>
+                        <v-card-text>
+                            <v-simple-table max-width="40">
+                                <tbody>
+                                    <tr>
+                                        <td colspan=2>
+                                            <v-text-field
+                                                v-model="search"
+                                                label="Zoek binnen resultaten"
+                                                hide-details
+                                                autofocus
+                                                clearable
+                                                dense></v-text-field>
+                                        </td>
+                                    </tr>
+                                    <tr
+                                        v-for="header in headers"
+                                        :key="header.text">
+                                        <td 
+                                            v-if="filters.hasOwnProperty(header.value)"
+                                            align="left">
+                                        {{header.text}} 
+                                        </td>
+                                        <td v-if="filters.hasOwnProperty(header.value)" class="text-left">
+                                            <v-select 
+                                                flat 
+                                                dense 
+                                                hide-details 
+                                                small 
+                                                multiple 
+                                                clearable 
+                                                :items="columnValueList(header.value)" 
+                                                v-model="filters[header.value]">     
+                                            </v-select>
+                                        </td>
+                                    </tr>
+                                </tbody>
+
+                            </v-simple-table>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols=12>
                     <v-card class="pa-1">
                         <v-card-title>
                             Audit hits
@@ -17,7 +65,7 @@
                         <v-card-text>
                             <v-data-table
                                 :headers="headers"
-                                :items="hits"
+                                :items="filteredHits"
                                 :items-per-page="5"
                                 :loading="loading"
                                 class="elevation-2"
@@ -48,15 +96,25 @@ export default {
                 { text: 'Task ID', value: 'task' },
                 { text: 'Project ID', value: 'project', align: ' d-none'  },
                 { text: 'Reden', value: 'reason' },
+                { text: 'Gebruiker', value: 'user' },
+                { text: 'Status', value: 'status' },
                 { text: 'Whitelist', value: 'ignore' },
                 { text: 'timestamp', value: 'timestamp' },
-            ]
+            ],
+            filters: {
+                user: [],
+                status: [],
+                ignore: [],
+            }
         }
     },
     methods: {
         selectTask(taskid, projectid){
             this.$router.push({ path: `/mapping/Projects/${projectid}/Task/${taskid}` });
-        }
+        },
+        columnValueList(val) {
+           return this.hits.map(d => d[val]).sort()
+        },
     },
     computed: {
         hits(){
@@ -67,7 +125,14 @@ export default {
         },
         user(){
             return this.$store.state.userData
-        }
+        },
+        filteredHits() {
+            return this.hits.filter(d => {
+                return Object.keys(this.filters).every(f => {
+                    return this.filters[f].length < 1 || this.filters[f].includes(d[f])
+                })
+            })
+        },
     },
     created() {
         this.$store.commit('MappingAudits/resetAuditHitsByProject')
