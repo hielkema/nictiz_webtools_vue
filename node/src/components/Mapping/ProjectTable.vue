52000b88a3f31f301741fdf4b963620b4780cb9a
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-06-24 14:02:21 +0200

    Projects per page increased

--- node/src/components/Mapping/ProjectTable.vue
+++ node/src/components/Mapping/ProjectTable.vue
@@ -11,7 +11,7 @@
                             <v-data-table
                                 :headers="headers"
                                 :items="Projects"
-                                :items-per-page="5"
+                                :items-per-page="15"
                                 :loading="loading"
                                 class="elevation-2"
                                 multi-sort
