f2b286e51ae4130e0fa8022afd0091d7fe270c31
root <user.name>
2020-11-26 15:31:14 +0100

    Fix property name

--- node/src/components/Mapping/MappingEditorECL1.vue
+++ node/src/components/Mapping/MappingEditorECL1.vue
@@ -524,7 +524,7 @@ export default {
             var payload = this.targets
             delete payload.allResults
             delete payload.mappings
-            delete payload.exclusion
+            delete payload.excluded
             payload.queries.forEach(function(query){ delete query.result });
 
             // First, save exclusions
