f7f070db0df10f1579bb4fb698612e8987658185
mertenssander <sander.mertens+github@gmail.com>
2021-02-11 14:35:06 +0100

    Audit hits in popup

--- node/src/components/Mapping/AuditList.vue
+++ node/src/components/Mapping/AuditList.vue
@@ -1,14 +1,66 @@
 <template>
     <div>
         <v-container 
-            v-if="user.groups.includes('mapping | access')"
-            fluid
+                v-if="user.groups.includes('mapping | access') && (audits_active.length > 0 || audits_whitelisted.length > 0 )"
             >
-            <v-btn @click="triggerAudit(selectedTask.id)">Trigger audit</v-btn>
-            <v-btn @click="getAudits(selectedTask.id)">Vernieuw QA hits</v-btn>
+            <v-card>
+                <v-toolbar
+                    color="cyan darken-2"
+                    dark
+                    dense>
+                    <v-toolbar-title>Quality assurance</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                    <v-chip
+                        v-if="audits_active.length > 0"
+                        class="ma-2"
+                        color="red"
+                        label
+                        text-color="white"
+                        @click="auditDetails = 'true'"
+                        >
+                        <v-icon left>
+                            mdi-alert-box-outline
+                        </v-icon>
+                        <span v-if="audits_active.length >1">
+                            {{audits_active.length}} QA waarschuwingen
+                        </span>
+                        <span v-if="audits_active.length == 1">
+                            {{audits_active.length}} QA waarschuwing
+                        </span>
+                        <v-divider/>
+                    </v-chip>
+                    
+                    <v-chip
+                        v-if="audits_whitelisted.length > 0"
+                        class="ma-2"
+                        color="green"
+                        label
+                        text-color="white"
+                        @click="auditDetails = 'true'"
+                        >
+                        <v-icon left>
+                            mdi-alert-box-outline
+                        </v-icon>
+                        <span v-if="audits_whitelisted.length >1">
+                            {{audits_whitelisted.length}} QA waarschuwingen met whitelist
+                        </span>
+                        <v-divider/>
+                        <span v-if="audits_whitelisted.length == 1">
+                            {{audits_whitelisted.length}} QA waarschuwing met whitelist
+                        </span>
+                        <v-divider/>
+                    </v-chip>
+                    <v-card-actions>
+                        <v-btn small @click="auditDetails = 'true'">Toon hits</v-btn>
+                        <v-spacer/>
+                        <v-btn small @click="triggerAudit(selectedTask.id)">Trigger audit</v-btn>
+                        <v-btn small @click="getAudits(selectedTask.id)">Vernieuw QA hits</v-btn>
+                    </v-card-actions>
+                </v-card-text>
+            </v-card>
         </v-container>
-
-        <v-container 
+        <!-- <v-container 
             v-if="user.groups.includes('mapping | access') && (audits_active.length > 0 || audits_whitelisted.length > 0 )"
             fluid
             >
@@ -33,13 +85,132 @@
                         <a @click="removeAudit(hit.id)" v-if="user.groups.includes('mapping | audit whitelist')">[remove]</a>
                     </v-alert>
             </div>
-        </v-container>
+        </v-container> -->
+
+
+
+        <!-- Modal for editing target -->
+        <v-dialog v-model="auditDetails" max-width="2000px">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">Audit hits</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols=12>
+                                <v-card>
+                                    <v-card-title>
+                                        Taken
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <v-simple-table max-width="40">
+                                            <tbody>
+                                                <tr>
+                                                    <td colspan=2>
+                                                        <v-text-field
+                                                            v-model="search"
+                                                            label="Zoek binnen resultaten"
+                                                            hide-details
+                                                            autofocus
+                                                            clearable
+                                                            dense></v-text-field>
+                                                    </td>
+                                                </tr>
+                                                <tr
+                                                    v-for="header in headers"
+                                                    :key="header.text">
+                                                    <td 
+                                                        v-if="filters.hasOwnProperty(header.value)"
+                                                        align="left">
+                                                    {{header.text}} 
+                                                    </td>
+                                                    <td v-if="filters.hasOwnProperty(header.value)" class="text-left">
+                                                        <v-select 
+                                                            flat 
+                                                            dense 
+                                                            hide-details 
+                                                            small 
+                                                            multiple 
+                                                            clearable 
+                                                            :items="columnValueList(header.value)" 
+                                                            v-model="filters[header.value]">     
+                                                        </v-select>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+
+                                        </v-simple-table>
+                                    </v-card-text>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols=12>
+                                <v-card class="pa-1">
+                                    <v-card-title>
+                                        Audit hits
+                                    </v-card-title>   
+                                    <v-card-text>
+                                        <v-data-table
+                                            :headers="headers"
+                                            :footer-props="pagination" 
+                                            :items="filteredHits"
+                                            :items-per-page="50"
+                                            :loading="loading"
+                                            :search="search"
+                                            class="elevation-2"
+                                            multi-sort
+                                            dense
+                                        >
+                                            <template v-slot:item.acties="{ item }">
+                                                <v-btn small @click="whitelistAudit(item.id)" v-if="user.groups.includes('mapping | audit whitelist')">Whitelist</v-btn>
+                                                <v-btn small @click="removeAudit(item.id)" v-if="user.groups.includes('mapping | audit whitelist')">Remove</v-btn>
+                                            </template>
+                                            <template v-slot:item.timestamp="{ item }">
+                                                {{Date(item.timestamp).toLocaleString()}}
+                                            </template>
+                                        </v-data-table>
+                                    </v-card-text>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="auditDetails = false">Sluiten</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
+            auditDetails: false,
+            search : '',
+            headers: [
+                { text: 'Project ID', value: 'project', align: ' d-none'  },
+                { text: 'Type melding', value: 'type' },
+                { text: 'Reden', value: 'reason' },
+                { text: 'Whitelist', value: 'ignore' },
+                { text: 'Sticky', value: 'sticky' },
+                { text: 'timestamp', value: 'timestamp' },
+                { text: '', value: 'acties' },
+            ],
+            pagination: {
+                "items-per-page-options": [25,50,100,150]
+            },
+            filters: {
+                user: [],
+                status: [],
+                ignore: [],
+                type: [],
+            }
         }
     },
     methods: {
@@ -58,6 +229,9 @@ export default {
         getAudits(id){
             this.$store.dispatch('MappingAudits/getAudits', id)
         },
+        columnValueList(val) {
+           return this.hits.map(d => d[val]).sort()
+        },
 
     },
     computed: {
@@ -75,6 +249,16 @@ export default {
         audits_whitelisted(){
             return this.audits.filter(function(item){
                 return item.ignore
+            })
+        },
+        hits(){
+            return this.$store.state.MappingAudits.audits
+        },
+        filteredHits() {
+            return this.hits.filter(d => {
+                return Object.keys(this.filters).every(f => {
+                    return this.filters[f].length < 1 || this.filters[f].includes(d[f])
+                })
             })
         },
         loading(){
